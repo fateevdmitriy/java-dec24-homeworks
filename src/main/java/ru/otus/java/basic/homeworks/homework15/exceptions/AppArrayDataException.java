ab5b9656e76361276704c7b859d9da0626e1df47
Fateev Dmitriy <FateevDV@digtp.com>
2025-02-24 00:43:54 +0300

    Минорные фиксы - домашнее задание урока 15.

--- src/main/java/ru/otus/java/basic/homeworks/homework15/exceptions/AppArrayDataException.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework15/exceptions/AppArrayDataException.java
@@ -1,6 +1,6 @@
 package ru.otus.java.basic.homeworks.homework15.exceptions;
 
-public class AppArrayDataException extends Exception {
+public class AppArrayDataException extends RuntimeException {
     
     public AppArrayDataException(int errorRow, int errorColumn) {
         super("[AppArrayDataException]: Ошибка преобразования типов данных в обрабатываемом массиве: строка " + errorRow + " столбец " 
--- src/main/java/ru/otus/java/basic/homeworks/homework15/exceptions/AppArraySizeException.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework15/exceptions/AppArraySizeException.java
@@ -1,6 +1,6 @@
 package ru.otus.java.basic.homeworks.homework15.exceptions;
 
-public class AppArraySizeException extends Exception {
+public class AppArraySizeException extends RuntimeException {
     
     public AppArraySizeException(int arrayDim) {
         super("[AppArraySizeException]: Ошибка в размерности обрабатываемого двумерного массива. Массив должен иметь размерность " 
