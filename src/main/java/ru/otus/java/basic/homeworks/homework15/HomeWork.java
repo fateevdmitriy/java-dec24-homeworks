ed076ca8717eac87a7b871a9ea2b22758085c940
Fateev Dmitriy <FateevDV@digtp.com>
2025-02-23 22:50:25 +0300

    Выполнено домашнее задание урока 15.

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework15/HomeWork.java
@@ -0,0 +1,43 @@
+package ru.otus.java.basic.homeworks.homework15;
+
+import ru.otus.java.basic.homeworks.homework15.exceptions.AppArrayDataException;
+import ru.otus.java.basic.homeworks.homework15.exceptions.AppArraySizeException;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        final int DIMENSION = 4;
+        int result;
+        String[][] strArray1 = {
+                {"10", "11", "12", "13"},
+                {"20", "21", "22", "23"},
+                {"30", "31", "32", "33"},
+                {"40", "41", "42", "43"},
+        };
+
+        try {
+            result = sumArrayElements(DIMENSION, strArray1);
+            System.out.println("Суммирование элементов массива выполнено успешно. Сумма = " + result + ".");
+        } catch (AppArraySizeException | AppArrayDataException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static int sumArrayElements(int arrDim, String[][] strArr) throws AppArraySizeException, AppArrayDataException {
+        if (strArr.length != arrDim || strArr[0].length != arrDim) {
+            throw new AppArraySizeException(arrDim);
+        }
+        int arrSum = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            for (int j = 0; j < strArr[i].length; j++) {
+                try {
+                    arrSum += Integer.parseInt(strArr[i][j]);
+                } catch (Exception e) {
+                    throw new AppArrayDataException(i + 1, j + 1);
+                }
+            }
+        }
+        return arrSum;
+    }
+
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework15/exceptions/AppArraySizeException.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.homeworks.homework15.exceptions;
+
+public class AppArraySizeException extends Exception {
+    
+    public AppArraySizeException(int arrayDim) {
+        super("[AppArraySizeException]: Ошибка в размерности обрабатываемого двумерного массива. Массив должен иметь размерность " 
+                + arrayDim + "x" + arrayDim + " элемента.");
+    }
+    
+}
