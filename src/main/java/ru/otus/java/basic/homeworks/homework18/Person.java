5674f242ad247a46bf942f0a00302336c5ac5476
Fateev Dmitriy <fateevdv@digtp.com>
2025-03-07 02:04:27 +0300

    Выполнена задача_1 домашнего задания урока 18

--- src/main/java/ru/otus/java/basic/homeworks/homework18/Person.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework18/Person.java
@@ -1,16 +1,16 @@
 package ru.otus.java.basic.homeworks.homework18;
 
 public class Person {
-    private final String name;
-    private final Position position;
-    private final Long id;
+    private String name;
+    private Position position;
+    private Long id;
 
     public Person(String name, Position position, Long id) {
         this.name = name;
         this.position = position;
         this.id = id;
     }
-    
+
     public String getName() {
         return name;
     }
