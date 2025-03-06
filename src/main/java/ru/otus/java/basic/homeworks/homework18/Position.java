13d9b449d2e64cc5cba3c386e018c7e84caedca6
Fateev Dmitriy <FateevDV@digtp.com>
2025-03-07 02:04:27 +0300

    Выполнена задача_1 домашнего задания урока 18

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework18/Position.java
@@ -0,0 +1,5 @@
+package ru.otus.java.basic.homeworks.homework18;
+
+public enum Position {
+    MANAGER, DIRECTOR, DRIVER, ENGINEER, SENIOR_MANAGER, DEVELOPER, QA, JANITOR, PLUMBER, BRANCH_DIRECTOR, JUNIOR_DEVELOPER
+}
