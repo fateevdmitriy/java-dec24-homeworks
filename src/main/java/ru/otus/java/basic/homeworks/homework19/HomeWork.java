fee6c17f934401c7901074757e7e2b87941d9ef9
Fateev Dmitriy <FateevDV@digtp.com>
2025-03-23 15:18:23 +0300

    Домашнее задание урока 19 - работа над ошибками.

--- src/main/java/ru/otus/java/basic/homeworks/homework19/HomeWork.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework19/HomeWork.java
@@ -2,36 +2,35 @@ package ru.otus.java.basic.homeworks.homework19;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class HomeWork {
-
     public static void main(String[] args) {
         listAllFilesInDir(".");
         String userFileName = requestUserForFileAndName();
-        OutputFileToConsole(userFileName);
+        outputFileToConsole(userFileName);
         appendUserInputToFile(userFileName);
-        OutputFileToConsole(userFileName);
+        outputFileToConsole(userFileName);
     }
-    
+
     public static void listAllFilesInDir(String pathToDir) {
         File folder = new File(pathToDir);
         if (!folder.isDirectory()) {
             System.out.println("Заданный путь не является корректным путем до существующей директории.");
             return;
         }
-        System.out.println("Текущая директория: " + folder.getAbsolutePath() + ".\nОна содержит следующие текстовые файлы:");
         FileFilter fileFilter = new FileFilter() {
             public boolean accept(File pathname) {
                 return pathname.getName().endsWith(".txt");
             }
         };
-        try {
-            for (File file : folder.listFiles(fileFilter)) {
-                System.out.printf("%s\n", file.getName());
-            }
-        } catch (NullPointerException npe) {
-            System.out.println("Произошла исключительная ситуация при попытке вывести список текстовых файлов в директоррии.");
-            npe.printStackTrace();
+        if (folder.listFiles(fileFilter) == null) {
+            System.out.println("Не удалось вывести список текстовых файлов в директории, проверьте существует ли директория.");
+            return;
+        }
+        System.out.println("Текущая директория: " + folder.getAbsolutePath() + ".\nОна содержит следующие текстовые файлы:");
+        for (File file : folder.listFiles(fileFilter)) {
+            System.out.printf("%s\n", file.getName());
         }
     }
 
@@ -39,15 +38,15 @@ public class HomeWork {
         String filename;
         System.out.println("Введите имя файла, с которым следует работать:");
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-            filename = reader.readLine();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            filename = bufferedReader.readLine();
             if (filename.isEmpty()) {
                 throw new IOException("Имя файла не может быть пустым.");
             }
             File usersFile = new File(filename);
             if (usersFile.exists()) {
                 if (usersFile.isFile()) {
-                    System.out.println("Файл с именем '" + filename + "' существует.");    
+                    System.out.println("Файл с именем '" + filename + "' существует.");
                 } else if (usersFile.isDirectory()) {
                     System.out.println("Файл с именем '" + filename + "' не существует, но сушествует директория с таким именем.");
                     return null;
@@ -59,53 +58,46 @@ public class HomeWork {
                 System.out.println("Файл с именем '" + filename + "' не существует.");
                 boolean isFileCreated = usersFile.createNewFile();
                 if (isFileCreated) {
-                    System.out.println("Создан новый файл с именем '"+filename+"'.");
+                    System.out.println("Создан новый файл с именем '" + filename + "'.");
                 } else {
-                    return null;                    
-                }                 
+                    return null;
+                }
             }
             return usersFile.getName();
         } catch (IOException ioe) {
-            ioe.getMessage();
             ioe.printStackTrace();
         }
         return null;
     }
 
-    public static void OutputFileToConsole(String filename) {
+    public static void outputFileToConsole(String filename) {
         StringBuilder sb = new StringBuilder();
-        try (InputStreamReader in = new InputStreamReader(new FileInputStream(filename))) {
-            int n = in.read();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
+            int n = bufferedReader.read();
             while (n != -1) {
-                sb.append((char)n);
-                n = in.read();
+                sb.append((char) n);
+                n = bufferedReader.read();
             }
             System.out.printf("Содержимое файла '%s':\n%s\n", filename, sb.toString());
         } catch (IOException ioe) {
-            ioe.getMessage();
             ioe.printStackTrace();
         }
     }
-    
+
     public static void appendUserInputToFile(String filename) {
-        String userStr;
         System.out.printf("Введите текстовую строку для записи в файл '%s'.\n", filename);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            userStr = reader.readLine();
-            if (!userStr.isEmpty()) {
-                try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename, true))) {
-                    userStr += System.lineSeparator();
-                    byte[] buffer = userStr.getBytes(StandardCharsets.UTF_8);
-                    out.write(buffer);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                System.out.println("Введена пустая строка, нечего записывать в файл.");
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        Scanner scanner = new Scanner(System.in);
+        String userStr = scanner.nextLine();
+        if (userStr.isEmpty()) {
+            System.out.println("Нечего записывать в файл, задана пустая строка для записи.");
+            return;
+        }
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename, true))) {
+            userStr += System.lineSeparator();
+            byte[] buffer = userStr.getBytes(StandardCharsets.UTF_8);
+            out.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    
 }
