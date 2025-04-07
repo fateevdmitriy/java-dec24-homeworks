104f4bda9f95e52a25a78a054c891651a32426f0
Fateev Dmitriy <FateevDV@digtp.com>
2025-04-07 09:29:50 +0300

    Домашнее задание урока 24 - правки по итогам ревью.

--- client/src/main/java/ru/otus/chat/Client.java
+++ client/src/main/java/ru/otus/chat/Client.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-``    
+
     public Client() throws IOException {
         try (Socket socket = new Socket("localhost", 8189);
              DataOutputStream out = new DataOutputStream(socket.getOutputStream());
