b7d0c1cb284337b9124a616dd0840becf2be4590
Fateev Dmitriy <FateevDV@digtp.com>
2025-04-26 22:52:42 +0300

    Домашнее задание урока 30 - работа над ошибками 2

--- src/main/java/ru/otus/java/basic/homeworks/homework30/HomeWork.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework30/HomeWork.java
@@ -3,40 +3,33 @@ package main.java.ru.otus.java.basic.homeworks.homework30;
 import java.util.concurrent.*;
 
 public class HomeWork {
-
+    private int counter = 0;
+    private final Object mon = new Object();
+    
     public static void main(String[] args) {
+        HomeWork homeWork = new HomeWork();
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        
-        for (int i = 0; i < 5; i++) {
-            Future<String> future1 = executorService.submit(new Callable<String>() {
-            @Override
-            public String call()  {                 
-                    return ("A");
-                }
-            });
-            Future<String> future2 = executorService.submit(new Callable<String>() {
-                @Override
-                public String call()  {
-                    return ("B");
-                }
-            });
-            Future<String> future3 = executorService.submit(new Callable<String>() {
-                @Override
-                public String call()  {
-                    return ("C");
-                }
-            });
-            
+        executorService.execute(() -> homeWork.printChar('A'));
+        executorService.execute(() -> homeWork.printChar('B'));
+        executorService.execute(() -> homeWork.printChar('C'));
+        executorService.shutdown();
+    }
+    
+    public void printChar(char symbol) {
+        synchronized (mon) {
+            int orderCnt = Character.toUpperCase(symbol) - 'A';
             try {
-                System.out.println(future1.get());
-                System.out.println(future2.get());
-                System.out.println(future3.get());
-            } catch (InterruptedException | ExecutionException e) {
+                for (int i = 0; i < 5; i++) {
+                    while (counter != i * 3 + orderCnt) {
+                        mon.wait();
+                     }
+                    System.out.println(symbol);
+                    counter++;
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        
-        executorService.shutdown();
     }
-
 }
